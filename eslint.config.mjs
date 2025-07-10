6ad61f2e568282c6714c71f623c6ddcaedd87cb0
jwren4170 <jwren4170@att.net>
2025-07-09 23:20:06 -0500

    Initial

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,22 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    plugins: ["unused-imports"],
+    rules: {
+      "no-unused-vars": "off",
+      "unused-imports/no-unused-imports": "warn",
+      "unused-imports/no-unused-vars": [
+        "warn",
+        {
+          vars: "all",
+          varsIgnorePattern: "^_",
+          args: "after-used",
+          argsIgnorePattern: "^_",
+        },
+      ],
+    },
+  },
 ];
 
 export default eslintConfig;
